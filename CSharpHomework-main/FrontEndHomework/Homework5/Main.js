e91f0b2f3bb7b37b71812df28750a3d5660f3e5a
Hsu <qw9596128@gmail.com>
2021-04-06 14:18:03 +0800

    homework20210406

---
+++ CSharpHomework-main/FrontEndHomework/Homework5/Main.js
@@ -0,0 +1,25 @@
+﻿
+function foo() {
+
+
+var init = function (arr) {
+    var table1 = document.getElementById("tableList");
+    
+    for (var i = 0; i < arr.length; i++) {
+        var newDom = document.createElement("tr");
+        var item = arr[i];
+        
+        var html =
+            '<td>' +
+            item.Name + "</td><td> " +
+            item.ISBN + "</td><td> $" +
+            item.Price + "</td>";
+        
+        newDom.innerHTML = html;
+        table1.appendChild(newDom);
+    }
+}
+
+    init(itemArr);
+   
+}
